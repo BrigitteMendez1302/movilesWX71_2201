8c70306b8cf561862e9a8465978362eb24c66ee0
BrigitteMendez1302 <U201914955@upc.edu.pe>
2022-04-26 00:21:47 -0500

    .

--- AppRecyclerView/app/src/main/java/com/example/apprecyclerview/MainActivity.kt
+++ AppRecyclerView/app/src/main/java/com/example/apprecyclerview/MainActivity.kt
@@ -2,14 +2,25 @@ package com.example.apprecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     var contacts = ArrayList<Contact>()
+
+    // creando el adapter
+    var contactAdapter = ContactAdapter(contacts)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         loadContacts()
+        initView()
+    }
+
+    private fun initView() {
+        val rvContact = findViewById<RecyclerView>(R.id.rvContact)
+        rvContact.adapter = contactAdapter
+        rvContact.layoutManager = LinearLayoutManager(this)
     }
 
     private fun loadContacts() {
